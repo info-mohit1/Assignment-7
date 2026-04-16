f7eaa37f2be8d8f046672f0a7fbe9fb6a6b23691
info-mohit1 <mdmohidhasan23@gmail.com>
2026-04-16 21:21:06 +0600

    feat: add FriendDetails page with check-in buttons and timeline save

---
+++ keenkeeper/src/App.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import FriendDetails from "./pages/FriendDetails";
+import Timeline from "./pages/Timeline";
+import Stats from "./pages/Stats";
+import NotFound from "./pages/NotFound";
+
